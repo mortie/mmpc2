f94437ce030db2685ef67fdcd12ab0dc42e91231
mortie <martid0311@gmail.com>
2016-08-21 16:25:52 +0200

    removed unnecessary variable

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ var fs = require("fs");
 var web = require("webstuff");
 var play = require("./js/play");
 
-var magnet = "magnet:?xt=urn:btih:13241fe16a2797b2a41b7822bde970274d6b687c&dn=Mad+Max%3A+Fury+Road+%282015%29+1080p+BrRip+x264+-+YIFY&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Fzer0day.ch%3A1337&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969";
-
 var conf = JSON.parse(fs.readFileSync("conf.json"));
 
 var app = web();
